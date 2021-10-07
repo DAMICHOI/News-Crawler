663659c66eef7b5cf20417011fe270b52a9f5650
DAMICHOI <choidami93@gmail.com>
2021-10-07 22:56:48 +0900

    style : 불필요한 주석 제거
    items.py 파일의 불필요한 주석 제거

--- NewsCrawler/NewsCrawler/items.py
+++ NewsCrawler/NewsCrawler/items.py
@@ -15,13 +15,3 @@ class NewscrawlerItem(scrapy.Item):
     newsUrl = scrapy.Field()    # 기사링크
     photoUrl = scrapy.Field()  # 사진링크
     pass
-
-    # name = scrapy.Field()
-    # source = scrapy.Field() # 신문사
-    # category = scrapy.Field()   # 카테고리
-    # title = scrapy.Field()  # 제목
-    # date = scrapy.Field()   # 날짜
-    # author = scrapy.Field() # 작성자
-    # article = scrapy.Field()    # 기사 내용
-    # url = scrapy.Field()    # 기사링크
-    # pass
